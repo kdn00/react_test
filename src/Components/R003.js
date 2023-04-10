d0f08583c7b464bc629a89cb571f65b514aa34c3
kdn00 <do7829@naver.com>
2023-04-10 18:01:22 +0900

    componentDidMount 함수

--- src/Components/R003.js
+++ src/Components/R003.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 
 class R003 extends Component{
+    // getDerivedStateFormProps(props, state) 함수는 constructor() 함수 다음으로 실행된다.
     static getDerivedStateFromProps(props, state){
         console.log('2. getDerivedStateFromProps Call : '+ props.prop_value);
-        return {};
+        return {tmp_state:props.prop_value};
     }
-    
+
+    // constructor(props) 함수는 생명주기 함수 중 가장 먼저 실행되며, 처음 한 번만 호출된다.
     constructor(props){
         super(props);
         this.state = {};
         console.log('1. constructor Call');
+    }
+
+    // 작성된 함수들 중 가장 마지막으로 실행된다.
+    // 화면이 모두 그려진 후에 실행돼야하는 이벤트 처리, 초기화 등에 가장 많이 활용된다.
+    componentDidMount() {
+        console.log('4. componentDidMount Call');
+        console.log('5. tmp_state : ' + this.state.tmp_state);
     }
 
     render(){
--- 개념 정리/React 시작하기.md
+++ 개념 정리/React 시작하기.md
@@ -43,3 +43,8 @@ static getDerivedStateFromProps(props, state){
     return {};
 }
 ```
+
+## componentDidMount()
+- 작성한 함수들 중 가장 마지막으로 실행된다.
+- render() 함수가 return되는 html 형식의 코드를 화면에 그려준 후 실행한다.
+- 화면이 모두 그려진 후에 실행돼야하는 이벤트 처리, 초기화 등에 가장 많이 활용된다.
