ad51d209694814dff4f8c9b896d28d41a803a4d0
kdn00 <do7829@naver.com>
2023-04-10 17:09:58 +0900

    컴포넌트 사용하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
+import TestComponent from './Components/R003';
 
 function App() {
   return (
     <div className="App">
       <h1>Start React 200!</h1>
-      <p>HTML 적용하기</p>
+      <p>css 적용하기</p>
+      <TestComponent />
     </div>
   );
 }
---
+++ src/Components/R003.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class R003 extends Component{
+    render(){
+        return(
+            <h2>[ THIS IS IMPORTED COMPONENT ]</h2>
+        )
+    }
+}
+
+export default R003;
